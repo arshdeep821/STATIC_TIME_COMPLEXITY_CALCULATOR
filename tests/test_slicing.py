20022cc6847717e05fb0f9e4294403e7dc788d73
Ilya Patrzykat <ilyapatrzykat@dhcp-206-87-154-235.ubcsecure.wireless.ubc.ca>
2024-04-04 21:59:26 -0700

    add another slicing test

--- tests/test_slicing.py
+++ tests/test_slicing.py
@@ -12,6 +12,10 @@ def example_slice_reverse(testList: List):
 def example_slice_complex(testList: List):
     return testList[4:5:-1]
 
+# O(n)
+def example_slice_just_first_num(testList: List):
+    return testList[2:]
+
 # O(1)
 def example_constant_test(testList: List):
     return testList[3]
