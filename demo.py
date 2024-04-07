4484cf4c169d64052094698b35d78da87a1ce73b
Arshdeep Jaggo <arshdeep.j2003@gmail.com>
2024-04-07 10:31:53 -0700

    Added more examples + fixed other examples

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 from typing import List
 
 # features: nested for loop
-# Expected: O(a(b(aloga)))
-def demo_for_loop(lst1: List, lst2: List):
+# Expected: O(a(b))
+def demo_for_loop(lst1: List, lst2: List) -> List:
 
     result = 0
 
@@ -15,7 +15,7 @@ def demo_for_loop(lst1: List, lst2: List):
 
 # features: if statement, slicing, math simplification (dropping constants)
 # Expected: O(aloga)
-def demo_if_statement(lst1: List, element: int):
+def demo_if_statement(lst1: List, element: int) -> List:
 
     lst = lst[::-1]
 
@@ -29,13 +29,99 @@ def demo_if_statement(lst1: List, element: int):
 
 # features: calling other functions
 # Expected: O(1)
-def function1(lst: List):
+def function1(lst: List) -> List:
     return lst[::-1]
 
 # Expected(aloga + b)
-def function2(arr: List) -> None:
+def function2(arr: List) -> List:
     
     new_lst = function1([x*2 for x in arr])
     arr.sort()
 
     return arr
+
+# features: Sets, for loops, if statements (LeetCode 217)
+# Expected: O(a)
+def containsDuplicate(nums: List) -> bool:
+    s = set()
+    for num in nums:
+        if num in s:
+            return True
+        s.add(num)
+    return False
+
+############## could also add this next option for sets
+
+# features: Set, for loop, if statement, while loop (LeetCode 3)
+# Expected: O(a)
+def lengthOfLongestSubstring(s: str) -> int:
+    l = 0
+    result = 0
+    curr_set = set()
+
+    for r in range(len(s)):
+        while s[r] in curr_set and l <= r:
+            curr_set.remove(s[l])
+            l += 1
+        curr_set.add(s[r])
+        result = max(result, r - l + 1)
+    return result
+
+# features: Dict, for loops, if statements (LeetCode 242)
+# Expected: O(a)
+def isAnagram(s: str, t: str) -> bool:
+    mapS = {}
+    mapT = {}
+
+    if len(s) != len(t):
+        return False
+    
+    for i in range(len(s)):
+        if s[i] in mapS:
+            mapS[s[i]] += 1
+        else:
+            mapS[s[i]] = 1
+    
+        if t[i] in mapT:
+            mapT[t[i]] += 1
+        else:
+            mapT[t[i]] = 1
+    
+    return mapS == mapT
+
+############## could also add this next option for sets
+
+# features: Dict, for loop, if statement (LeetCode 424)
+# Expected: O(a)
+def characterReplacement(s: str, k: int) -> int:
+    count = {}
+    
+    l = 0
+    maxf = 0
+    for r in range(len(s)):
+        count[s[r]] = 1 + count.get(s[r], 0)
+        maxf = max(maxf, count[s[r]])
+
+        if (r - l + 1) - maxf > k:
+            count[s[l]] -= 1
+            l += 1
+
+    return (r - l + 1)
+
+# features: String Input, Local Stack Data Structure, for loop, if statement
+# Expected: O(a)
+def isValid(s: str) -> bool:
+    stack = []
+    match = {"}": "{", ")": "(", "]":"["}
+
+    for b in s:
+        if b in match:
+            if len(stack) != 0 and stack[-1] == match[b]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(b)
+
+    return len(stack) == 0
+
